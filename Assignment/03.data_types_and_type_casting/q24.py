18f5934e1b1e275fe4488454e5e86a49382a5e0c
ojasvaagarwal <ojasva.agarwal.cg@gmail.com>
2026-08-24 17:29:54 +0530

    add q24

---
+++ Assignment/03.data_types_and_type_casting/q24.py
@@ -0,0 +1,6 @@
+student_age = 18
+student_height = 5.8
+student_name = "Rahul"
+is_student = True
+result = None
+print(type(student_name),type(student_age),type(student_height),type(is_student),type(result))
