78d8360657e5bf60104c4602be380c3554dca30a
ojasvaagarwal <ojasva.agarwal.cg@gmail.com>
2026-08-24 17:33:08 +0530

    add q29

---
+++ Assignment/03.data_types_and_type_casting/q29.py
@@ -0,0 +1,7 @@
+product_name = "shoe"
+product_quantity = 3000
+product_price = 40.00
+product_availability = True
+product_discount = None
+
+print(type(product_availability),type(product_discount),type(product_name),type(product_price),type(product_quantity))
