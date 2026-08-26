1c8f32e74474b14638f5c8b83b9b6c462a2206fe
ojasvaagarwal <ojasva.agarwal.cg@gmail.com>
2026-08-26 07:44:25 +0530

    add task3

---
+++ Assignment/04.arithmetic_operators/task3.py
@@ -0,0 +1,9 @@
+sub_1 = 85
+sub_2 = 90
+sub_3 = 75
+
+total_marks = sub_1 + sub_2 + sub_3
+average_marks = total_marks / 3
+
+print("Total Marks:", total_marks)
+print("Average Marks:", average_marks)
