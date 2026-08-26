79ae92e0387576f7d72578385ec91623ac32f77a
ojasvaagarwal <ojasva.agarwal.cg@gmail.com>
2026-08-26 07:45:30 +0530

    add task6

---
+++ Assignment/04.arithmetic_operators/task6.py
@@ -0,0 +1,11 @@
+# Positive numbers
+a = 14
+b = 4
+print("Positive / :", a / b)
+print("Positive //:", a // b)
+
+# Negative numbers
+a = -14
+b = 4
+print("Negative / :", a / b)
+print("Negative //:", a // b)
