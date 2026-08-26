b63419d78dd322b43d739473e92abf4a7601ca69
ojasvaagarwal <ojasva.agarwal.cg@gmail.com>
2026-08-26 07:44:10 +0530

    add task2

---
+++ Assignment/04.arithmetic_operators/task2.py
@@ -0,0 +1,11 @@
+a = 10
+b = 2.5
+
+# Operations between int and float return float
+print("Addition:", a + b, type(a + b))
+print("Subtraction:", a - b, type(a - b))
+print("Multiplication:", a * b, type(a * b))
+print("Division:", a / b, type(a / b))
+print("Floor Division:", a // b, type(a // b))
+print("Modulus:", a % b, type(a % b))
+print("Exponentiation:", a ** b, type(a ** b))
