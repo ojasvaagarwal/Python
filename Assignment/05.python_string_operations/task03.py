cf2944908f7f96048f5e5fe5b0384e00fe8059e5
ojasvaagarwal <ojasva.agarwal.cg@gmail.com>
2026-08-31 17:01:44 +0530

    task added

---
+++ Assignment/05.python_string_operations/task03.py
@@ -0,0 +1,7 @@
+a="Python Programming"
+print(a[:]) # Complete string
+print(len(a)) # Length
+print(a[0]) # First character
+print(a[-1]) # Last character
+print(a[2]) # Third character
+print(a[-2]) # Second-last character
